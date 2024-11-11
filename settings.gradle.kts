a4bf3e0e105225c3b8c739cd5be6131fad07ab75
Tendekayi Taruvinga <162187403+iMCode1@users.noreply.github.com>
2024-11-11 13:00:56 +0200

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -18,6 +18,11 @@ dependencyResolutionManagement {
         mavenCentral()
     }
 }
+versionCatalogs {
+        create("libs") {
+            from(files("gradle/libs.versions.toml"))
+        }
+}
 
 rootProject.name = "Student-Study-App"
 include(":app")
