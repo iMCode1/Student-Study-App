9ce978d226bc0b12b9db07181da83fce71ee382b
Asi <64479526+MachNacho@users.noreply.github.com>
2024-11-12 08:00:04 +0200

    Update LeaderboardResponse.kt
    Fixed crash when the json is returned

--- app/src/main/java/com/example/student_study_app/models/LeaderboardResponse.kt
+++ app/src/main/java/com/example/student_study_app/models/LeaderboardResponse.kt
@@ -2,6 +2,6 @@ package com.example.student_study_app.models
 
 data class LeaderboardResponse(
     val username: String,
-    val score: Int,
+    val score:Double,
     val timeTakenSeconds: Int
 )
