fde30e1c5b2052ce148d51e57e9c4381fcbddcdc
Tendekayi Taruvinga <162187403+iMCode1@users.noreply.github.com>
2024-11-12 19:10:50 +0200

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    id("com.google.dagger.hilt.android") version "2.51.1" apply true
+    id("kotlin-kapt")
 }
 
 android {
